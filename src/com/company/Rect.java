a32e4f138d20b084a5bb8d12561c48bd6434fc37
narges <jatages@gmail.com>
2021-03-17 18:49:02 +0330

    Implement rectangle getArea func

--- src/com/company/Rect.java
+++ src/com/company/Rect.java
@@ -2,11 +2,15 @@ package com.company;
 
 public class Rect {
 
+    private int height;
+    private  int width;
 
     public Rect(int height,int width) {
+        this.height = height;
+        this.width = width;
     }
 
     public int getArea(){
-        return 0;
+        return this.height*this.width;
     }
 }
