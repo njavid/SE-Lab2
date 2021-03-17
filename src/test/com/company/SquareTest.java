2536dad7a82c3a0ca05937827dd7999f6c76301a
Ehyaee <z.ehyaee96@gmail.com>
2021-03-17 19:33:40 +0330

    Implement test for Square getArea function

--- src/test/com/company/SquareTest.java
+++ src/test/com/company/SquareTest.java
@@ -1,11 +1,17 @@
 package test.com.company;
 
+import com.company.Rect;
+import com.company.Square;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SquareTest {
-    @BeforeEach
-    void setUp() {
+
+    @Test
+    void testGetArea(){
+        Square square = new Square(4);
+        assertEquals(16,square.getArea());
     }
 }
